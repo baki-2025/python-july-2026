5cdf6da846ca9df0fcacebc57d9fd8a78a2f11a6
Abdullah Baki <abz.147570@gmail.com>
2026-07-09 21:31:59 +0600

    indentation/variables

--- first.py
+++ first.py
@@ -1,2 +1,14 @@
 # Print a greeting message
 print("Hello, World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+# Indentation
+if 5 > 2:
+  print("Five is greater than two!")
+#  Variables
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
