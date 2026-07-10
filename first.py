73f4135138324777b24aa0a7731434cb798f55ea
Abdullah Baki <abz.147570@gmail.com>
2026-07-10 11:44:48 +0600

    number/math/mix

--- first.py
+++ first.py
@@ -6,9 +6,20 @@ print("Learning Python is fun!")
 # Indentation
 if 5 > 2:
   print("Five is greater than two!")
+
 #  Variables
 x = 5
 y = "Hello, World!"
-
 print(x)
 print(y)
+
+# numbers
+print(3)
+print(358)
+print(50000)
+# math operations
+print(3 + 3)
+print(2 * 5)
+# string operations
+print("I am", 35, "years old.")
+
