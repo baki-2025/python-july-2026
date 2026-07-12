4ccc5b4eecfb7bd56e46623df79c65560569145c
Abdullah Baki <abz.147570@gmail.com>
2026-07-12 15:50:45 +0600

    variables

---
+++ variables.py
@@ -0,0 +1,22 @@
+#legal variable names
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+# # #illegal variable names
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
+# # #This example will produce an error in the result
