7719d8c6d62e328de1d06b7e47af212434667561
Abdullah Baki <abz.147570@gmail.com>
2026-07-18 12:59:07 +0600

    global var

---
+++ global-variable.py
@@ -0,0 +1,39 @@
+#1
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+#2
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+#global variable with keyword
+#1
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+#2
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
--- variables.py
+++ variables.py
@@ -21,9 +21,30 @@ print(myvar2)
 
 # # #This example will produce an error in the result
 
+# Many Values to Multiple Variables
+# Python allows you to assign values to multiple variables in one line:
+
 x, y, z = "Orange", "Banana", "Cherry"
 
 print(x)
 print(y)
 print(z)
 
+# One Value to Multiple Variables
+# And you can assign the same value to multiple variables in one line:
+
+x = y = z = "Orange"
+
+print(x)
+print(y)
+print(z)
+
+# Unpack a Collection
+# If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
